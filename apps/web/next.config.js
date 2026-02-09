33c9e44051a941b76cdda508b0a6f48f1f11150b
Safi <sssafiullahhh@gmail.com>
2026-02-09 12:45:22 +0500

    created vercel.json

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ["@recipevault/shared"],
+    experimental: {
+        forceSwcTransforms: true,
+    },
 };
 
 module.exports = nextConfig;
