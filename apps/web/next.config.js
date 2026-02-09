ac22c45238fbb260469e5cb06e415e15b862326a
Safi <sssafiullahhh@gmail.com>
2026-02-09 13:45:51 +0500

    reverted

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ["@recipevault/shared"],
-    experimental: {
-        forceSwcTransforms: true,
-    },
-    swcMinify: false,
 };
 
 module.exports = nextConfig;
